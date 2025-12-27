7e6706fc0e508a5590abaa82216400d7120d7062
MD YUSUF MASOOD <mdyusufmasood7488@gmail.com>
2025-12-28 01:18:29 +0530

    First question in DSA series.

---
+++ src/Q_01.java
@@ -0,0 +1,21 @@
+
+public class Q_01 {
+    public static void main(String[] args) {
+
+
+        // Q.No. 1920. Build Array from Permutation
+
+        int[] num = {0,2,1,5,3,4};
+
+        int[] ans = new int[num.length];
+
+        for(int i = 0; i < ans.length; i++){
+            ans[i] = num[num[i]];
+        }
+
+        for (int val : ans){
+            System.out.print(val + " ");
+        }
+
+    }
+}
