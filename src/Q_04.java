f12cbbb97139b497d1f9511b117a704c2246d948
MD YUSUF MASOOD <mdyusufmasood7488@gmail.com>
2025-12-28 17:14:35 +0530

    Question 4th.

---
+++ src/Q_04.java
@@ -0,0 +1,25 @@
+public class Q_04 {
+    public static void main(String[] args) {
+
+
+        //Q.No. 1672. Richest Customer Wealth
+
+
+        int[][] account = {{1,2,3},{3,2,1}};
+
+        int welath = 0;
+
+        for(int i = 0; i < account.length; i++){
+            int money = 0;
+            for(int j = 0; j < account[i].length; j++){
+                money += account[i][j];
+            }
+
+            if(money > welath){
+                welath = money;
+            }
+        }
+
+        System.out.println(welath);
+    }
+}
