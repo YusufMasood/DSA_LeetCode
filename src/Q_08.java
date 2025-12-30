fc142169c1fcf047b4911f5c964a4e115810e7cb
MD YUSUF MASOOD <mdyusufmasood7488@gmail.com>
2025-12-30 13:08:35 +0530

    8th Question

---
+++ src/Q_08.java
@@ -0,0 +1,28 @@
+public class Q_08 {
+    public static void main(String[] args) {
+
+
+        //Q.No. 1365. How Many Numbers Are Smaller Than the Current Number
+
+
+
+        int[] nums ={8,1,2,2,3};
+        int[] smallerNums  = new int[nums.length];
+
+        for(int i=0;i<smallerNums.length;i++){
+            int count = 0;
+            for(int j=0;j<nums.length;j++){
+                if(nums[i] > nums[j] && i != j){
+                    count++;
+                }
+            }
+
+            smallerNums[i] = count;
+        }
+
+        for(int value:smallerNums){
+            System.out.print(value + " ");
+        }
+
+    }
+}
