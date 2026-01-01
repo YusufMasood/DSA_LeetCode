14c8df05af109408da46ce58b3e8807aa058d190
MD YUSUF MASOOD <mdyusufmasood7488@gmail.com>
2026-01-01 13:36:21 +0530

    9th Question

---
+++ src/Q_09.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q_09 {
+    public static void main(String[] args) {
+
+
+        //Q.No. 1389. Create Target Array in the Given Order
+
+
+        int[] nums = {0,1,2,3,4};
+        int[] index = {0,1,2,2,1};
+
+        List<Integer> target = new ArrayList<>();
+
+        for(int i = 0; i < nums.length; i++){
+            target.add(index[i],nums[i]);
+        }
+
+        System.out.print(target +" ");
+    }
+}
