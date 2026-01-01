216f09f9185ae5dab6a527846585ab04afc1da3f
MD YUSUF MASOOD <mdyusufmasood7488@gmail.com>
2026-01-01 14:06:05 +0530

    10th Question

---
+++ src/Q_10.java
@@ -0,0 +1,34 @@
+public class Q_10 {
+    public static void main(String[] args) {
+
+
+
+        //Q.No. 1832. Check if the Sentence Is Pangram
+
+
+
+
+        String sentence = "thequickbrownfoxjumpsoverthelazydog";
+
+        boolean[] appeared = new boolean[26];
+
+        for(int i = 0; i < sentence.length(); i++){
+            char c = sentence.charAt(i);
+
+            if(c >= 'a' && c <= 'z'){
+                appeared[c - 'a'] = true;
+            }
+
+        }
+
+        for(boolean val : appeared){
+            if(!val){
+                System.out.println(false);
+                return;
+            }
+        }
+
+        System.out.println(true);
+
+    }
+}
