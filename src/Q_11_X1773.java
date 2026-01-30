28e4da70b01bc6aa3b4cd17d79e21c126cd171b6
MD YUSUF MASOOD <mdyusufmasood7488@gmail.com>
2026-01-30 18:01:42 +0530

    Solved 11th question in the series

--- src/Q_11_X1773.java
+++ src/Q_11_X1773.java
@@ -5,6 +5,26 @@ public class Q_11_X1773 {
         //Q.No. 1773. Count Items Matching a Rule
 
 
+        String [][] item = {{"Phone", "silver", "Iphone"},
+                            {"Laptop", "Black", "Mackbook"},
+                            {"phone", "Black", "Pixel"},
+                            {"laptop", "Golden", "Asus"}};
+
+        String ruleKey = "color";
+        String ruleVlaue = "Black";
+
+        int index = ruleKey.equals("type") ? 0 : ruleKey.equals("color") ? 1 : 2;
+
+        int count = 0;
+
+        for(int i = 0; i < item.length; i++){
+            if(item[i][index].equals(ruleVlaue)){
+                count++;
+            }
+        }
+
+
+        System.out.println(count);
 
     }
 }
